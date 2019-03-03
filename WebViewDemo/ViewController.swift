10f1a2fd205ad52ed8b9e595fa9007df96eadea6
Ricardo <eehaha@gmail.com>
2019-03-03 11:29:25 +0800

    queue network task

--- WebViewDemo/ViewController.swift
+++ WebViewDemo/ViewController.swift
@@ -14,8 +14,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        let url = URL(string: "https://www.stackoverflow.com")!
-        webView.loadRequest(URLRequest(url:url))
+        if  let url = URL(string: "https://www.stackoverflow.com"){
+            let request = NSMutableURLRequest(url:url)
+            let task = URLSession.shared.dataTask(with: request as URLRequest){
+                data, response, error in
+                if let error = error{
+                    print(error)
+                }else{
+                    if let unwrappedData = data{
+                        let dataString  = NSString(data: unwrappedData, encoding: String.Encoding.utf8.rawValue)
+                        print(dataString)
+                        DispatchQueue.main.sync(execute:
+                            {}
+                        )
+                    }
+                }
+            }
+            task.resume()
+        }
+        
         
     }
 
